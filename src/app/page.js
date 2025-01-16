ef29334be2587ffdb07c885314edf9d3022a9529
Fahad Ahmad <fahmad01@innovation-insight.com>
2025-01-16 13:11:10 +0500

    inline editor

--- src/app/page.js
+++ src/app/page.js
@@ -1,101 +1,76 @@
+'use client';
+
 import Image from "next/image";
+import { useEffect } from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  const emailHeaderConfig = {
+    selector: '.tinymce-heading',
+    menubar: false,
+    inline: true,
+    plugins: [
+      'lists',
+      'powerpaste',
+      'autolink'
+    ],
+    toolbar: 'undo redo | bold italic underline',
+    valid_elements: 'strong,em,span[style],a[href]',
+    valid_styles: {
+      '*': 'font-size,font-family,color,text-decoration,text-align'
+    },
+    powerpaste_word_import: 'clean',
+    powerpaste_html_import: 'clean',
+  };
+  
+  const emailBodyConfig = {
+    selector: '.tinymce-body',
+    menubar: false,
+    inline: true,
+    plugins: [
+      'link', 'lists', 'powerpaste',
+      'autolink', 'tinymcespellchecker'
+    ],
+    toolbar: [
+      'undo redo | bold italic underline | fontfamily fontsize',
+      'forecolor backcolor | alignleft aligncenter alignright alignfull | numlist bullist outdent indent'
+    ],
+    valid_elements: 'p[style],strong,em,span[style],a[href],ul,ol,li',
+    valid_styles: {
+      '*': 'font-size,font-family,color,text-decoration,text-align'
+    },
+    powerpaste_word_import: 'clean',
+    powerpaste_html_import: 'clean',
+  };
+  
+  useEffect(() => {
+    tinymce.init(emailHeaderConfig);
+    tinymce.init(emailBodyConfig);
+  }, []);
+
+  return (
+    <div class="demo-inline">
+  <div class="container">
+    {/* <p><img src="../_images/tiny-banner.png" style={{width: '100%'}} /></p> */}
+    <h1 class="tinymce-heading">The world&rsquo;s first rich text editor in the cloud</h1>
+    <p><img src="https://www.tiny.cloud/images/image-two.jpg" /></p>
+    <div class="tinymce-body">
+      <p>Have you heard about Tiny Cloud? It&rsquo;s the first step in our journey to help you deliver great content creation experiences, no matter your level of expertise. 50,000 developers already agree. They get free access to our global CDN, image proxy services and auto updates to the TinyMCE editor. They&rsquo;re also ready for some exciting updates coming soon.</p>
+      <p>One of these enhancements is <strong>Tiny Drive</strong>: imagine file management for TinyMCE, in the cloud, made super easy. Learn more at <a href="../../../tinydrive/">tiny.cloud/tinydrive</a>, where you&rsquo;ll find a working demo and an opportunity to provide feedback to the product team.</p>
     </div>
+  </div>
+  <h2 class="tinymce-heading">An editor for every project</h2>
+  <div class="tinymce-body">
+    <p>Here are some of our customer&rsquo;s most common use cases for TinyMCE:</p>
+    <ul>
+    <li>Content Management Systems (<em>e.g. WordPress, Umbraco</em>)</li>
+    <li>Learning Management Systems (<em>e.g. Blackboard</em>)</li>
+    <li>Customer Relationship Management and marketing automation (<em>e.g. Marketo</em>)</li>
+    <li>Email marketing (<em>e.g. Constant Contact</em>)</li>
+    <li>Content creation in SaaS systems (<em>e.g. Eventbrite, Evernote, GoFundMe, Zendesk</em>)</li>
+    </ul>
+    <p>And those use cases are just the start. TinyMCE is incredibly flexible, and with hundreds of APIs there&rsquo;s likely a solution for your editor project. If you haven&rsquo;t experienced Tiny Cloud, get started today. You&rsquo;ll even get a free trial of our premium plugins &ndash; no credit card required!</p>
+  </div>
+</div>
   );
 }
