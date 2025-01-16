92c154b79ed993d21b46474364a58dcae9642928
Fahad Ahmad <fahmad01@innovation-insight.com>
2025-01-16 13:41:35 +0500

    multi pages

---
+++ app/blogs/page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Blogs = () => {
+    return(
+        <div>
+            <h1>Blogs Page</h1>
+            <p>This is the Blogs Page.</p>
+        </div>
+    )
+}
+
+export default Blogs;
